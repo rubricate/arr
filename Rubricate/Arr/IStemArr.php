976f0b5570ad9105ca26f32848d49642b8f24ae0
Estefanio N Santos <estefanions@gmail.com>
2026-06-30 17:55:26 -0300

    [#2606301722] refactor(arr): deprecate IStemArr contract interface
    - Mark IStemArr as deprecated using formal PHPDoc block annotations.
    - Refactor interface to extend ISupportArr for backward compatibility.
    - Schedule total structural removal of legacy contract for v5.0.0.
    
    Ref: #v4.1.0

--- Rubricate/Arr/IStemArr.php
+++ Rubricate/Arr/IStemArr.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Rubricate\Arr;
 
+/**
+ * @deprecated since v4.1.0, use ISupportArr instead. Will be removed in v5.0.0.
+ */
 interface IStemArr extends IGetArr, IKeyCount
 {
 }
